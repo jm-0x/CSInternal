15a0e82929b24fb1c772afe57ba398226be17f68
Jon <44406427+jm-0x@users.noreply.github.com>
2023-10-11 23:00:34 +0200

    initial project setup

---
+++ Architecture/src/Math.cpp
@@ -0,0 +1,25 @@
+#include "Math.h"
+
+
+namespace MathLibrary
+{
+    double Arithmetic::Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    double Arithmetic::Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    double Arithmetic::Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    double Arithmetic::Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
